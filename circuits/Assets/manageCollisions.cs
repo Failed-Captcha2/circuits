c7440f1bf686e86227b2749fb9eb5a4e56054e00
Failed-Captcha <crowe.elshobary@gmail.com>
2025-03-18 19:27:45 -0400

    wire collisions update
    on collision with wire, wire contacts are added to contacts list
    TODO: update on each collision with wire

--- circuits/Assets/manageCollisions.cs
+++ circuits/Assets/manageCollisions.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class manageCollisions : MonoBehaviour
 {
     public GameObject[] contacts;
+    public GameObject[] wireContacts;
 
     // Start is called before the first frame update
     void Start()
@@ -18,38 +19,47 @@ public class manageCollisions : MonoBehaviour
         
     }
 
-    void addContact(GameObject contact)
+    public void addContact(GameObject contact)
     {
+        //create copy of original array
+        GameObject[] contactscopy = new GameObject[contacts.Length];
+        for (int i = 0; i < contacts.Length; i++)
+        {
+            contactscopy[i] = contacts[i];
+        }
+
         if (contact.tag == "wire")
         {
+            //get opposite wire terminal contacts
+            wireContacts = contact.GetComponentInParent<wire>().getContacts(contact);
 
+            if (wireContacts != null)
+            {
+                //add wire contacts to contacts list
+                for (int i = 0; i < wireContacts.Length; i++)
+                {
+                    addContact(wireContacts[i]);
+                }
+            }
         }
-        else
+            else
+            {
+                //increment size of array
+                contacts = new GameObject[contacts.Length + 1];
+
+                //add new contact to original array
+                contacts[contacts.Length - 1] = contact;
+            }
+
+        //copy back all objects into original array
+        for (int i = 0; i < contactscopy.Length; i++)
         {
-
-            //create copy of original array
-            GameObject[] contactscopy = new GameObject[contacts.Length];
-            for (int i = 0; i < contacts.Length; i++)
-            {
-                contactscopy[i] = contacts[i];
-            }
-
-            //increment size of array
-            contacts = new GameObject[contacts.Length + 1];
-
-            //copy back all objects into original array
-            for (int i = 0; i < contactscopy.Length; i++)
-            {
-                contacts[i] = contactscopy[i];
-            }
-
-            //add new contact to original array
-            contacts[contacts.Length - 1] = contact;
+            contacts[i] = contactscopy[i];
         }
 
     }
 
-    void removeContact(GameObject contact)
+    public void removeContact(GameObject contact)
     {
         //create copy of original array
         GameObject[] contactscopy = new GameObject[contacts.Length];
--- circuits/Assets/wire.cs
+++ circuits/Assets/wire.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class wire : MonoBehaviour
 {
     public float current;
+    public GameObject[] terminals;
     //TODO: move all collision boxes of wire contacts to the same spot
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,9 +19,22 @@ public class wire : MonoBehaviour
 
     }
 
+    public GameObject[] getContacts(GameObject terminal)
+    {
+        if (terminal == terminals[0])
+        {
+            return terminals[1].GetComponent<manageCollisions>().contacts;
+        }
+        else if (terminal == terminals[1])
+        {
+            return terminals[0].GetComponent<manageCollisions>().contacts;
+        }
+        Debug.Log("get contacts failed!" + terminal.name);
+        return null;
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
-
+        collision.gameObject.GetComponent<manageCollisions>().addContact(gameObject);
     }
 }
